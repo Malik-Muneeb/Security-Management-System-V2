690b3211302b54fb9b242f2a1e0ba298dd07c69e
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-03-20 23:52:49 +0500

    Write some lines of code of login and connect database

--- login.php
+++ login.php
@@ -1,12 +1,13 @@
+<?php require('conn.php');
+$error="";?>
 <html>
 <head>
 <title> Login </title>
 <link rel="stylesheet" type="text/css" href="styles.css">
-<script src="SecurityManager.js"></script>
+    <script src="jquery-3.2.1.min.js" type="text/javascript"></script>
 </head>
 <script>
 function main() {
-    
     var btn=document.getElementById('btnLogin');
     btn.onclick = function() {
         var login=document.getElementById('txtLogin').value;
@@ -15,57 +16,46 @@ function main() {
             alert("Enter Login Name first!!!");
         else if(password=="")
             alert("Enter Password!!!");
-        else
-            {
-                 var result=SecurityManager.ValidateAdmin(login,password);
-                if(result==true)
-                    window.location.href="adminHome.php";
-                else
-                    alert("Invalid UserName/Password");
-            }
-       
-        
-    }
-    var btn1=document.getElementById('btnLogin1');
-    btn1.onclick= function(){
-    	var userObjArr=SecurityManager.GetAllUsers();
-    	var login=document.getElementById('txtLogin1').value;
-        var password=document.getElementById('txtPassword1').value;
-        if(login=="")
-            alert("Enter Login Name first!!!");
-        else if(password=="")
-            alert("Enter Password!!!");
-        else
-            {
-                var isExist=false;
-                var name;
-                for(var i=0; i<userObjArr.length && !isExist; i++)
-                {
-                    if(userObjArr[i].login==login && userObjArr[i].password==password)
-                       {
-                           isExist=true;
-                           name=userObjArr[i].name; 
-                       } 
-                }
-                if(isExist)
-                {
-                    window.location.href="userHome.php"//?name="+login;
-                    localStorage.setItem('name', name);//to set value
-                }
-                else
-                    alert("Invalid UserName/Password");
-            }
     }
 }
 </script>
+
+<?php
+
+    if(isset($_POST["btnLogin"]))
+    {
+        $login=$_POST["txtLogin"];
+        $password=$_POST["txtPassword"];
+        $sql="SELECT * FROM users WHERE login='".$login."' And password='".$password."' ";
+        $result = mysqli_query($conn, $sql);
+        $recordsFound = mysqli_num_rows($result);
+        if ($recordsFound > 0) {
+            //Step-4: Iterate row by row
+            while($row = mysqli_fetch_assoc($result)) {  //ye associative array ki trhan ha
+
+                $id = $row["login"];
+                $name = $row["password"];
+                $userid=$row["isadmin"];
+                //Step-5: Display values
+                echo "login: $id, password:$name, $userid";
+            }
+        }
+        else
+        {
+            $error="Invalid login or Password";
+        }
+    }
+
+?>
 
 <body class="bodyContainer" onload="main();">
     <h1 style="background-color:#006600;color:white;float;margin-right:1050px">Security Manager</h1>
     <div class="container" style="float:left;">
         <h1>Admin Login</h1>
-            <form method="post" action="">
+            <form method="post" action="login.php">
                 Login: <input id="txtLogin" name="txtLogin" /> <br>
                 Password: <input id="txtPassword" name="txtPassword" type="password" /> <br>
+                <span id="errorSpan" style="color:red"><?php echo ($error);?></span>
                 <input type="submit" name="btnLogin" id="btnLogin" value="Login" />
                 <input class ="cancelbtn" type="reset" value="cancel" />
             </form>
