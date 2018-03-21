a2a64e3dd873c8af31add44b6cc3a74e42d22ae7
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-03-21 18:22:28 +0500

    Almost complete login form

--- login.php
+++ login.php
@@ -1,64 +1,73 @@
-<?php require('conn.php');
-$error="";?>
+<?php
+session_start();
+require('conn.php');
+$error = "";
+$login=""?>
 <html>
 <head>
-<title> Login </title>
-<link rel="stylesheet" type="text/css" href="styles.css">
+    <title> Login </title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <script src="jquery-3.2.1.min.js" type="text/javascript"></script>
 </head>
 <script>
-function main() {
-    var btn=document.getElementById('btnLogin');
-    btn.onclick = function() {
-        var login=document.getElementById('txtLogin').value;
-        var password=document.getElementById('txtPassword').value;
-        if(login=="")
-            alert("Enter Login Name first!!!");
-        else if(password=="")
-            alert("Enter Password!!!");
+    function main() {
+        var btn = document.getElementById('btnLogin');
+        btn.onclick = function () {
+            var login = document.getElementById('txtLogin').value;
+            var password = document.getElementById('txtPassword').value;
+            if (login == "") {
+                alert("Enter Login Name first!!!");
+                return false;
+            }
+            else if (password == "") {
+                alert("Enter Password!!!");
+                return false;
+            }
+            return true;
+        }
+
+        $("#reset").click(function(){
+            $('loginForm')[0].reset();
+        });
     }
-}
 </script>
 
 <?php
 
-    if(isset($_POST["btnLogin"]))
-    {
-        $login=$_POST["txtLogin"];
-        $password=$_POST["txtPassword"];
-        $sql="SELECT * FROM users WHERE login='".$login."' And password='".$password."' ";
+if (isset($_POST["btnLogin"])) {
+    $login = $_POST["txtLogin"];
+    $password = $_POST["txtPassword"];
+    if ($login == "" && $password = "")
+        $error = "You missed login or Password";
+    else {
+        $sql = "SELECT * FROM users WHERE login='" . $login . "' And password='" . $password . "' ";
         $result = mysqli_query($conn, $sql);
         $recordsFound = mysqli_num_rows($result);
         if ($recordsFound > 0) {
-            //Step-4: Iterate row by row
-            while($row = mysqli_fetch_assoc($result)) {  //ye associative array ki trhan ha
-
-                $id = $row["login"];
-                $name = $row["password"];
-                $userid=$row["isadmin"];
-                //Step-5: Display values
-                echo "login: $id, password:$name, $userid";
-            }
-        }
-        else
-        {
-            $error="Invalid login or Password";
-        }
+            $row = mysqli_fetch_assoc($result);
+            $isAdmin = $row["isadmin"];
+            $_SESSION["user"]=$row["login"];
+            if($userid==1)
+                header("Location: home.php?id=".$isAdmin);
+            else
+                header("Location: home.php?id=".$isAdmin);
+        } else
+            $error = "Invalid login or Password";
     }
-
+}
 ?>
 
 <body class="bodyContainer" onload="main();">
-    <h1 style="background-color:#006600;color:white;float;margin-right:1050px">Security Manager</h1>
-    <div class="container" style="float:left;">
-        <h1>Admin Login</h1>
-            <form method="post" action="login.php">
-                Login: <input id="txtLogin" name="txtLogin" /> <br>
-                Password: <input id="txtPassword" name="txtPassword" type="password" /> <br>
-                <span id="errorSpan" style="color:red"><?php echo ($error);?></span>
-                <input type="submit" name="btnLogin" id="btnLogin" value="Login" />
-                <input class ="cancelbtn" type="reset" value="cancel" />
-            </form>
-    </div>
+<h1 style="background-color:#006600;color:white;float;margin-right:1050px">Security Manager</h1>
+<div class="container" style="float:left;">
+    <h1>Admin Login</h1>
+    <form name="loginForm" method="post" action="login.php">
+        Login: <input id="txtLogin" name="txtLogin" value="<?php echo ($login); ?>"/> <br>
+        Password: <input id="txtPassword" name="txtPassword" type="password"/> <br>
+        <span id="errorSpan" style="color:red"><?php echo($error); ?></span>
+        <input type="submit" name="btnLogin" id="btnLogin" value="Login"/>
+        <input class="cancelbtn" type="reset" value="cancel" id="btnReset"/>
+    </form>
+</div>
 </body>
 </html>
