9b9df0049da9dd62b9660aff6d3342c025c1086c
muneeb52 <bcsf15m030@pucit.edu.pk>
2018-03-23 11:45:34 +0500

    handle save and edit functionality

---
+++ updateDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_POST["btnSave"])) {
+    $updateId=$_GET["edit"];
+    $login=$_POST["txtLogin"];
+    $password=$_POST["txtPassword"];
+    $name=$_POST["txtName"];
+    $email=$_POST["txtEmail"];
+    $country=$_POST["cmbCountries"];
+    if(isset($_POST["isAdmin"]))
+        $isAdmin=1;
+    else
+        $isAdmin=0;
+    $sql = "UPDATE users set login='".$login."', password='".$password."', name='".$name."',".
+             "email='".$email."', countryid='".$countryId."', createdon='".$createdOn."', ".
+            "createdby='".$createdBy."', isadmin=$isAdmin where userid=$updateId";
+    if (mysqli_query($conn, $sql)) {
+        ?><script>alert("Record updated successfully");</script><?php
+    } else {
+        ?><script>alert("Error updating record");</script><?php
+    }
+}
+?>
--- userManagement.php
+++ userManagement.php
@@ -2,10 +2,16 @@
 session_start();
 require('conn.php');
 $login=""; $password=""; $name="";
-$email=""; $error="";
-$editId=$_GET["edit"];
-if($editId)
-    include ("editDAO.php");
+$email=""; $error=""; $editId=0;
+if(isset($_GET["edit"])) {
+    $editId=$_GET["edit"];
+    echo $editId;
+    if($_GET["edit"]) {
+        include ("editDAO.php");
+        include ("updateDAO.php");
+    }
+}
+
 ?>
 <html>
 <head>
@@ -52,7 +58,8 @@ function main() {
 }
 </script>
 <?php
-include ("userAddDAO.php");
+if($editId==0)
+    include ("userAddDAO.php");
 ?>
 <body onload="main();">
 <?php
@@ -61,19 +68,19 @@ if($_SESSION["isAdmin"]==1)
 ?>
 
 <div>
-	<form class="container1" method="POST" name="userForm" style="float:left;">
+	<form class="container1" method="POST" name="userForm" action="userManagement.php?edit=<?php echo $editId;?>" style="float:left;">
         <h1>Users</h1>
         <span>Login: </span> <input type="text" id="txtLogin" name="txtLogin" value="<?php echo ($login);?>"><br>
         <span>Password: </span> <input type="password" id="txtPassword" name="txtPassword" value="<?php echo ($password);?>" ><br>
         <span>Name: </span> <input type="text" id="txtName" name="txtName" value="<?php echo ($name);?>"><br>
         <span>Email: </span> <input type="email" id="txtEmail" name="txtEmail" value="<?php echo ($email);?>"><br>
         <span>Country: </span> <select name="cmbCountries" id="cmbCountries">
-            <option <?php if($countryId==0){ ?> selected <?php } ?> value="0">--Select--</option>
-            <option <?php if($countryId==1){ ?> selected <?php } ?> value="1">Pakistan</option>
-            <option <?php if($countryId==2){ ?> selected <?php } ?> value="2">India</option>
-            <option <?php if($countryId==3){ ?> selected <?php } ?> value="3">China</option>
+            <option <?php if(isset($countryId) && $countryId==0){ ?> selected <?php } ?> value="0">--Select--</option>
+            <option <?php if(isset($countryId) && $countryId==1){ ?> selected <?php } ?> value="1">Pakistan</option>
+            <option <?php if(isset($countryId) && $countryId==2){ ?> selected <?php } ?> value="2">India</option>
+            <option <?php if(isset($countryId) && $countryId==3){ ?> selected <?php } ?> value="3">China</option>
         </select><br>
-        <br><input <?php if($isAdmin==1){ ?> checked <?php } ?> type="checkbox" name="isAdmin" style="margin-left: -130px;" >
+        <br><input <?php if(isset($isAdmin) && $isAdmin==1){ ?> checked <?php } ?> type="checkbox" name="isAdmin" style="margin-left: -130px;" >
         <Span style="margin-left: -130px;"><b>Is He/She Admin?</b></b></Span><br>
         <span id="errorSpan" style="color:red"><?php echo($error); ?></span>
         <input type="submit" id="btnSave" name="btnSave" value="Save">
